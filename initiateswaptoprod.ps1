2dff9b4a58de8a3cd9f81190ba2df865164e4e8b
dariagrigoriu <gitlabsunshine>
2015-09-09 18:49:16 -0700

    swap and multi-swap

---
+++ initiateswaptoprod.ps1
@@ -0,0 +1,16 @@
+﻿param(
+  [string]$ResourceGroup,
+  [string]$SlotName
+)
+
+Switch-AzureMode -Name AzureResourceManager
+CLS
+
+# Apply target config to deployment slots
+$ParametersObject = @{
+	targetSlot  = "production"
+}
+
+Invoke-AzureResourceAction -ResourceGroupName $ResourceGroup -ResourceType Microsoft.Web/sites/slots -ResourceName $SlotName -Action applySlotConfig -Parameters $ParametersObject -ApiVersion 2015-07-01 -Force -Verbose
+Write-Host "Production slot specific configuration elements applied"
+ 
---
+++ swaptoprod.ps1
@@ -0,0 +1,16 @@
+﻿param(
+  [string]$ResourceGroup,
+  [string]$SlotName
+)
+
+Switch-AzureMode -Name AzureResourceManager
+CLS
+
+# Swap deployment slots
+$ParametersObject = @{
+	targetSlot  = "production"
+}
+
+Write-Host "Swap operation in progress"
+Invoke-AzureResourceAction -ResourceGroupName $ResourceGroup -ResourceType Microsoft.Web/sites/slots -ResourceName $SlotName -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-07-01 -Force
+Write-Host "Swap operation completed"
